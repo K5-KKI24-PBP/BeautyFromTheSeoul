e4fa94364da205f8cfcab2cd8d817e68d6c2fc38
luticakep <kaylasorayadj@gmail.com>
2024-10-22 23:29:56 +0700

    create edit & delete function

--- events/models.py
+++ events/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+import uuid
+from django.core.exceptions import ValidationError
 
 class Events(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=200)
     description = models.TextField()
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
     location = models.CharField(max_length=200)
     promotion_type = models.CharField(max_length=200)
+
