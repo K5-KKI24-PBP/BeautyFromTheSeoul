1da06fa908b2f12ec7e605853f3731384187575b
thataruby <thata@dfi.id>
2024-10-24 00:26:20 +0700

    styled landing, added CRUD for ads

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from main.views import show_main, create_ad
+from main.views import show_main, create_ad, delete_ad, edit_ad
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('create_ad/', create_ad, name='create_ad'),
+    path('delete/<uuid:id>', delete_ad, name='delete_ad'),
+    path('edit-ad/<uuid:id>', edit_ad, name='edit_ad'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
+from django.http import HttpResponseRedirect
 from main.forms import AdForm
 from main.models import AdEntry
 
@@ -19,3 +20,20 @@ def create_ad(request):
     else:
         form = AdForm()
     return render(request, 'create_ads.html', {'form': form})
+
+def delete_ad(request, id):
+    ad = AdEntry.objects.get(pk = id)
+    ad.delete()
+    return HttpResponseRedirect(reverse('main:show_main'))
+
+def edit_ad(request, id):
+    ad = AdEntry.objects.get(pk = id)
+
+    form = AdForm(request.POST or None, instance=ad)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "edit_ad.html", context)
