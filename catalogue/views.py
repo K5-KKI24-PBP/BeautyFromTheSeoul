298e12dcd529b08dd273f8df04391dd563e6c31c
GhazaFadhlilbaqi <fadhlilbaqimuhammadghaza@gmail.com>
2024-10-23 20:29:36 +0700

    Fixed authentication, added admin view for catalogue

--- catalogue/views.py
+++ catalogue/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render, redirect
 from catalogue.forms import AddProductForm, ProductFilterForm
 from catalogue.models import Products
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseForbidden
 from django.urls import reverse
+from functools import wraps
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def superuser_required(view_func):
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        if request.user.is_superuser:
+            return view_func(request, *args, **kwargs)
+        else:
+            return HttpResponseForbidden("Access denied. You must be a superuser to access this page.")
+    return _wrapped_view
+
 def show_products(request):
     products = Products.objects.all()
     form = ProductFilterForm(request.GET)
@@ -25,6 +36,8 @@ def show_products(request):
     return render(request, "catalogue.html", context)
 
 # Add Product
+@superuser_required
+@login_required
 def add_product(request):
     form = AddProductForm(request.POST or None)
 
@@ -39,6 +52,8 @@ def add_product(request):
 
 
 # Editing product
+@superuser_required
+@login_required
 def edit_product(request):
     product = Products.objects.get()
 
@@ -52,6 +67,8 @@ def edit_product(request):
     return render(request, "edit_product.html", context)
 
 # Delete Product
+@superuser_required
+@login_required
 def delete_product():
     product = Products.objects.get()
     
