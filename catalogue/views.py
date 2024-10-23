f3b49f0efd7d710ccaf0e1659bc1d4ebb457d759
skibidiyo <diyobanu@gmail.com>
2024-10-23 11:52:45 +0700

    views dot peewhy

--- catalogue/views.py
+++ catalogue/views.py
@@ -1,13 +1,60 @@
 from django.shortcuts import render, redirect
-from catalogue.forms import AddProductForm
+from catalogue.forms import AddProductForm, ProductFilterForm
 from catalogue.models import Products
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def show_products(request):
     products = Products.objects.all()
+    form = ProductFilterForm(request.GET)
+    
+    if form.is_valid():
+        product_name = form.cleaned_data.get['name']
+        brand = form.cleaned_data.get['brand']  
+        
+        if product_name:
+            products = products.filter(name__icontains=product_name)
+        if brand:
+            products = products.filter(brand__icontains=brand)
 
     context = {
-        'products': products
-    }
+        'products': products}
+    return redirect(request, "catalogue.html", context)
 
-    return render(request, 'catalogue.html', context)
+# Add Product
+def add_product(request):
+    form = AddProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        product_entry = form.save(commit=False)
+        product_entry.user = request.user
+        product_entry.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_product.html", context)
+
+
+# Editing product
+def edit_product(request):
+    product = Products.objects.get()
+
+    form = Products(request.POST or None, instance=product)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "edit_product.html", context)
+
+# Delete Product
+def delete_product():
+    product = Products.objects.get()
+    
+    product.delete()
+
+    return HttpResponseRedirect(reverse('main:show_main'))
+
+
