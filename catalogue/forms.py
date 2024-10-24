3d9a0fc2a789c3a22eaa2371f75fb8adb6934d60
Muhammad Jordan <jordan.aziz18@gmail.com>
2024-10-24 10:04:49 +0700

    connect dataset

--- catalogue/forms.py
+++ catalogue/forms.py
@@ -1,12 +1,13 @@
 from django import forms
 from catalogue.models import Products
+from django.forms import ModelForm
 
 class ProductFilterForm(forms.Form):
-    brand = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder': 'Brand'}))
+    product_brand = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder': 'Brand'}))
     product_type = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder': 'Product Type'}))
 
 
 class AddProductForm(forms.ModelForm):
     class Meta:
         model = Products
-        fields = ['name', 'brand', 'product_type', 'description', 'price']
+        fields = ['product_name', 'product_brand', 'product_type', 'product_description', 'price']
--- catalogue/models.py
+++ catalogue/models.py
@@ -3,10 +3,10 @@ import uuid
 
 # Create your models here.
 class Products(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=100)
-    brand = models.CharField(max_length=100)
+    product_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    product_name = models.CharField(max_length=100)
+    product_brand = models.CharField(max_length=100)
     product_type = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    product_description = models.TextField()
+    price = models.CharField(max_length=100)
 
