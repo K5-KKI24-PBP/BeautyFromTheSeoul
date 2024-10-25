442d2d2210c020646fb0843ecc2c4b7ec5654a44
wuyu0107 <mkim050107@gmail.com>
2024-10-26 00:21:26 +0700

    Configure add/remove product from favorites

--- favorites/urls.py
+++ favorites/urls.py
@@ -4,7 +4,7 @@ from favorites.views import show_favorites, add_favorites, remove_favorites
 app_name = 'favorites'
 
 urlpatterns = [
-    path('add/<int:product_id>/', add_favorites, name="add_favorites"),
-    path('remove/<int:product_id>/',remove_favorites, name="remove_favorites"),
+    path('add/<uuid:product_id>/', add_favorites, name="add_favorites"),
+    path('remove/<uuid:product_id>/',remove_favorites, name="remove_favorites"),
     path('', show_favorites, name='favorites'),
 ]
